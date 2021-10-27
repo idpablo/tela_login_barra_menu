dd91c63f8fcd5277d37dc21351dd39ffca6152c8
Pablo Bento Soares <51011413+idpablo@users.noreply.github.com>
2021-10-27 08:36:15 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,15 +1,3 @@
-/*Script ocultar menu*/
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function(){
-    var currentScrollPos = window.pageYOffset;
-    if(prevScrollpos > currentScrollPos){
-        document.getElementById("navbar").style.top = "0";
-    } else {
-        document.getElementById("navbar").style.top = "-50px";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
 /*Script para efeito nos campos login e senha*/
 
 var email = document.getElementById('email');
@@ -29,3 +17,16 @@ password.addEventListener('focus',()=>{
 password.addEventListener('blur',()=>{
     password.style.borderColor = "color: rgb(32, 29, 29);";
 });
+
+/*Script ocultar menu*/
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function(){
+    var currentScrollPos = window.pageYOffset;
+    if(prevScrollpos > currentScrollPos){
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
